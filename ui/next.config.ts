b0e64c8d87f0536f6c346b694ac8c777a740bc9c
skords83 <skords@ik.me>
2026-06-21 16:50:11 +0200

    Update next.config.ts

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -4,13 +4,14 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'crumb.skords.de' },
-      { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: '**.wordpress.com' },
-      { protocol: 'https', hostname: '**.wp.com' },
-      { protocol: 'https', hostname: 'ploetzblog.de' },
-      { protocol: 'https', hostname: '**.ploetzblog.de' },
-      { protocol: 'http', hostname: 'localhost' },
+      { protocol: "https", hostname: "crumb.skords.de" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "**.wordpress.com" },
+      { protocol: "https", hostname: "**.wp.com" },
+      { protocol: "https", hostname: "ploetzblog.de" },
+      { protocol: "https", hostname: "**.ploetzblog.de" },
+      { protocol: "https", hostname: "webimages.we2p.de" },
+      { protocol: "http", hostname: "localhost" },
     ],
   },
 };
